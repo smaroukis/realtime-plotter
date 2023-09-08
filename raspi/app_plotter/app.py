7b58c9e23e2225904761b485a578305dac67ad15
Spencer <smaroukis@gmail.com>
2023-09-08 16:55:05 +0900

    matplotlib animation function in Plotter class

--- raspi/app_plotter/app.py
+++ raspi/app_plotter/app.py
@@ -1,29 +1,19 @@
-# 2023-09-07 
-# issue with control flow / blocking when trying to put it in a class
-# looking to recreate digikey post
-# maybe an issue with scope, threading, ? 
-
+# NEXT - merge app.py 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
-import time
 
-class Plotter(object):
-    """Class for plotting."""
+class Plotter:
+
     def __init__(self):
-        # Clear other plots
-        plt.close('all')
+        self.x = []
+        self.y = []
 
-        # Init Data
-        # self.ax_x_lim = [0, 0]
-        # self.ax_y_lim = [0, 0]
-        self.x_data, self.y_data = [], []
-        self.interval = 1000 # set to update every 1 second
-        self.frames = 100
-
-        # Init Plot
         self.fig, self.ax = plt.subplots()
-        self.ani = None
+        self.line, = self.ax.plot([], [], lw=2)
+        self.ax.set_xlim(0, 2*np.pi) 
+        self.ax.set_ylim(-1, 1)
+        self.ani = FuncAnimation(self.fig, self.update, interval=100) 
 
     def __del__(self):
         pass
@@ -31,62 +21,25 @@ class Plotter(object):
         #     print("closing plot")
         #     self.fig.close()
 
-    # Not in use
-    def init_plot(self):
-        self.scatter = self.ax.scatter([], [])
-        return self.scatter,
-
-    # Not in use
     def store_datapoint(self, x, y):
         """ Store data in a list
         Separates the ingestion of data from the plotting of data"""
-        self.x_data.append(x)
-        self.y_data.append(y)
+        self.x.append(x)
+        self.y.append(y)
 
-    # TODO - make a buffer to remove old data points
-    # Q: Is frames needed as i? 
     def update(self, i):
-        """ Function that is passed to animation.FuncAnimation.
-        frame increments by one each time the function is called."""
-        # optionally slice e.g. xs=self.x_data[-50:]
-        print("y({}) = {}".format(self.x_data[i], self.y_data[i]))
-
+        # Update Data
+        x_i = 2*np.pi*i/100
+        y_i = np.sin(x_i)
+        self.store_datapoint(x_i, y_i)
+        
         # Update Plot
-        # array_2d = np.c_[self.x_data, self.y_data]
-        self.ax.clear()
+        if i > 1: # fix error for numpy of one element
+            self.ax.set_xlim([min(self.x), max(self.x)])
+            self.ax.set_ylim([min(self.y), max(self.y)])
 
-        # TODO - need to figure out the control, 
-        # HACK FOR EXAMPLE INCREMENT
-        xs = self.x_data[:i]
-        ys = self.y_data[:i]
-        self.ax.plot(xs, ys) 
-        # self.scatter.set_offsets(array_2d) # create 2D array with data in vertical columns
-        return self.scatter,
+        self.line.set_data(self.x, self.y)
+        return self.line, 
 
-    # TODO - add blitting for better performance 
-    def animate(self):
-        # Note on frames: If frames is not passed it continues indefinitely
-        ani = FuncAnimation(self.fig, self.update, frames=self.frames, interval = self.interval)
-        plt.show()
-        return ani   
-
-##  ---------------- MAIN -------------------------
-##  ---------------- MAIN -------------------------
-
-# Test Plotter
 plotter = Plotter()
-plotter.x_data = np.linspace(0, 10, 100)
-plotter.y_data = np.sin(plotter.x_data)
-plt.ion()
-
-ani = plotter.animate()
 plt.show()
-
-# TODO - do we need a thread to run this and the for loop at the same time? 
-# Try as global scope:
-# ani = FuncAnimation(plotter.fig, plotter.update, frames=plotter.frames, init_func=plotter.init_plot, interval = plotter.interval)
-# OR  as class funcction
-# plotter.animate() # will update based on plotter.interval
-# Issue is the plotter blocks? need a queue? or threading?
-
-# Alternatively: have the animate/update functioVn call the sensor for data
